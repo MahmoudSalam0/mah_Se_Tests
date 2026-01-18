a40b60ef3e35d9fa798fbae20fe28993d79ada9a
Mahmoud Abdalsalam <msalam@smail.ucas.edu.ps>
2026-01-18 17:56:34 +0200

    add variables to class std

--- std.java
+++ std.java
@@ -1,5 +1,8 @@
 public class std {
     public static void main(String[] args) throws java.io.IOException {
+public String name;
+public String email:
+public int Phone number;
 
 public void displayinfo()
     }
